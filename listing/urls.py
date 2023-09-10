0ca0f8adedcac7bc33ca36890cf9cadf006ac270
Afeef K K <45afeef@gmail.com>
2023-09-10 07:39:56 +0530

    Created Route, View and Templates for Listing

--- listing/urls.py
+++ listing/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 name = 'listing'
 urlpatterns = [
-    path('',views.index)
+    path('',views.index),
+    path('a',views.ListingListView.as_view(), name="listing-list"),
+    path('a/<int:pk>',views.ListingDetailView.as_view(), name="listing-detail")
 ]
--- listing/views.py
+++ listing/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import DetailView, ListView
+
+from .models import * 
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello from Listing App")
+
+class ListingListView(ListView):
+    model = Listing
+
+class ListingDetailView(DetailView):
+    model = Listing
