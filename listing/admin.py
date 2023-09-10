c5926b848039df0866b916b89ca3c60020ee4cda
Afeef K K <45afeef@gmail.com>
2023-09-10 07:31:57 +0530

    Added Models and displyed in admin pannel

--- listing/admin.py
+++ listing/admin.py
@@ -1,3 +1,81 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
+from .models import *
 
 # Register your models here.
+
+
+class PriceInline(admin.TabularInline):
+    model = Price
+    extra = 1
+
+
+class MediaInline(admin.TabularInline):
+    model = Media
+    extra = 1
+    exclude = ('media_type',)  # Exclude media_type field from form
+    readonly_fields = ('preview',)
+
+
+@admin.register(Media)
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ('media_type', 'preview',"file_url")
+
+    def file_url(self,obj):
+        return obj.file.url
+    
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'display_medias','get_price')
+    list_filter = ('name',)
+    search_fields = ('name',)
+    inlines = [PriceInline,MediaInline]
+
+    def display_medias(self, obj):
+        html = '<img src="{}" style="max-width: 50px; max-height: 50px;" />'
+        return format_html(''.join(html.format(media.file.url) for media in obj.media_set.all() if media.media_type == "Image" ))
+    display_medias.short_description = 'Medias'
+
+    def get_price(self,obj):
+        price = Price.objects.filter(listing=obj).first()
+        if price:
+            return "{}/{}".format(price.saleprice, price.unit.symbol)
+        else:
+            return 'N/A'
+    get_price.short_description = 'Price/Unit'
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name','display_medias','get_price')    
+    inlines = [PriceInline,MediaInline]
+
+    def display_medias(self, obj):
+        html = '<img src="{}" style="max-width: 50px; max-height: 50px;" />'
+        return format_html(''.join(html.format(media.file.url) for media in obj.media_set.all() if media.media_type == "Image"))
+    display_medias.short_description = 'Medias'
+
+    def get_price(self,obj):
+        price = Price.objects.filter(listing=obj).first()
+        if price:
+            return "{}/{}".format(price.saleprice, price.unit.symbol)
+        else:
+            return 'N/A'
+    get_price.short_description = 'Price/Unit'
+
+
+@admin.register(Currency)
+class CurrecyAdmin(admin.ModelAdmin):
+    list_display = ('name','symbol','code')
+
+
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+    list_display=('name','symbol','unit_type','conversion_factor')
+
+
+admin.site.register([
+    # Listing
+ ])
--- listing/models.py
+++ listing/models.py
@@ -1,3 +1,88 @@
 from django.db import models
+from django.utils.html import format_html
 
-# Create your models here.
+class Price(models.Model):
+    mrp = models.DecimalField(max_digits=10, decimal_places=2)
+    saleprice = models.DecimalField(max_digits=10, decimal_places=2)
+    offerprice = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.ForeignKey('Currency', on_delete=models.CASCADE)
+    listing = models.ForeignKey('Listing', on_delete=models.CASCADE)
+    unit = models.ForeignKey('Unit', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Price: {self.saleprice} {self.currency.code} per {self.unit.symbol}'
+
+class Media(models.Model):
+    MEDIA_TYPES = (
+        ('Image', 'Image'),
+        ('Video', 'Video'),
+    )
+    media_type = models.CharField(max_length=5, choices=MEDIA_TYPES)
+    file = models.FileField(upload_to='media/')
+    listing = models.ForeignKey('Listing', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'Media: {self.media_type}'
+    
+    def preview(self):
+        if self.media_type == 'Video':
+            return format_html('<video width="150" controls><source src="{}" type="video/mp4"></video>', self.file.url)
+        elif self.media_type == 'Image':
+            return format_html('<img src="{}" width="150" />', self.file.url)
+        else:
+            return 'Media type not supported in preview'
+    preview.short_description = 'Preview Of Media'
+
+
+
+class Listing(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    class Meta:
+        abstract = False
+    
+    def __str__(self):
+        return self.name
+
+
+class Product(Listing):
+    height = models.DecimalField(max_digits=10, decimal_places=2)
+    weight = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Service(Listing):
+    available_at = models.DateTimeField()
+    not_available_at = models.DateTimeField()
+    
+    def __str__(self):
+        return self.name
+
+
+class Currency(models.Model):
+    code = models.CharField(max_length=3)
+    symbol = models.CharField(max_length=3)
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
+class Unit(models.Model):
+    UNIT_TYPES = (
+        ('Quantity', 'Quantity'),
+        ('Weight', 'Weight'),
+        ('Volume', 'Volume'),
+        ('Length', 'Length'),
+        ('Time', 'Time'),
+        ('Area', 'Area'),
+    )
+    name = models.CharField(max_length=200)
+    symbol = models.CharField(max_length=3)
+    unit_type = models.CharField(max_length=8, choices=UNIT_TYPES)
+    conversion_factor = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
